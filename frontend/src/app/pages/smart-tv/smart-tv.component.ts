c71c1c1f9ba9cd3a87ccad80bfa052ef48a4fc1d
Eleni <csd3846@csd.uoc.gr>
2019-11-17 23:57:01 +0200

    Smart Tv almost ready (without players) - just need to center the titles

--- frontend/src/app/pages/smart-tv/smart-tv.component.ts
+++ frontend/src/app/pages/smart-tv/smart-tv.component.ts
@@ -19,6 +19,7 @@ export class SmartTvComponent implements OnInit {
   background_color: string;
   next_up_icon: string;
   round_title_path: string;
+  background_rect: string;
 
   constructor() {
     this.phase = Phase.Day;
@@ -29,6 +30,7 @@ export class SmartTvComponent implements OnInit {
     this.count = 1;
     this.upper_icon_path = "Sun";
     this.next_up_icon = "nu_secret_voting";
+    this.background_rect = "tv-rectangle-day";
     //this.round_counter++;
   }
 
@@ -43,6 +45,7 @@ export class SmartTvComponent implements OnInit {
       this.next_up_icon = "nu_doctor";
       this.round_title_path = "mafia-voting";
       this.count++;
+      this.background_rect = "tv-rectangle-night";
       //this.round_counter++;
     } else {//night->day
       this.phase = Phase.Day;
@@ -52,6 +55,8 @@ export class SmartTvComponent implements OnInit {
       this.upper_icon_path = "Sun";
       this.background_color = "#E67E22";
       this.round_title_path = "open-ballot";
+      this.background_rect = "tv-rectangle-day";
+      this.next_up_icon = "nu_secret_voting";
       //this.round_counter++;
     }
   }
@@ -59,15 +64,47 @@ export class SmartTvComponent implements OnInit {
   changeRound() {
     if (this.phase == Phase.Day) {
       switch (this.phase_title) {
-        case this.dround[0]:
-
+        case this.dround[0]: //Open Ballot -> Secret Voting
+          this.background_rect = "sec-vot-rect-day";
+          this.round_title_path = "secret-voting";
+          this.phase_title = this.dround[1];
+          this.next_title = this.nround[0];
+          this.next_up_icon = "nu_mafia";
           break;
-        case this.dround[1]:
-
+        case this.dround[1]: //Secret Voting -> Mafia Voting
+          this.changePhase();
           break;
       }
     } else {
-
+      switch (this.phase_title) {
+        case this.nround[0]: //Mafia Voting -> Doctor
+          this.background_rect = "sec-vot-rect-day";
+          this.round_title_path = "doctor";
+          this.phase_title = this.nround[1];
+          this.next_title = this.nround[2];
+          this.next_up_icon = "nu_detective";
+          this.background_rect = "tv-rectangle-night";
+          break;
+        case this.nround[1]: //Doctor -> Detective
+          this.background_rect = "sec-vot-rect-day";
+          this.round_title_path = "detective";
+          this.phase_title = this.nround[2];
+          this.next_title = this.nround[3];
+          this.next_up_icon = "nu_barman";
+          this.background_rect = "tv-rectangle-night";
+          break;
+        case this.nround[2]: //Detective -> Barman
+          this.background_rect = "sec-vot-rect-day";
+          this.round_title_path = "barman";
+          this.phase_title = this.nround[3];
+          this.next_title = this.dround[0];
+          this.next_up_icon = "nu_open_ballot";
+          this.background_rect = "tv-rectangle-night";
+          break;
+        case this.nround[3]: //Barman -> Open Ballot
+          this.changePhase();
+          break;
+      }
     }
   }
 
