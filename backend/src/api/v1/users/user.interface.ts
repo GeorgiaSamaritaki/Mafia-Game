4ea438fa36b357159d7fa8d33814abbd4944168f
Georgia Samaritaki <csd3840@csd.uoc.gr>
2019-11-18 16:19:49 +0200

    changed assets, fixing titles

--- backend/src/api/v1/users/user.interface.ts
+++ backend/src/api/v1/users/user.interface.ts
@@ -1,44 +1,44 @@
 export interface User {
     name: string;
     role: string;
-    avatar_path: string;
+    avatar_path: string; //path in the assets file 
 }
 
 var users: Array<User> = [
     {
         name: "Alice",
         role: "",
-        avatar_path: ""
+        avatar_path: "player1.png"
     },
     {
         name: "Maria",
         role: "",
-        avatar_path: ""
+        avatar_path: "player3.png"
     },
     {
         name: "George",
         role: "",
-        avatar_path: ""
+        avatar_path: "player2.png"
     },
     {
         name: "Kiki",
         role: "",
-        avatar_path: ""
+        avatar_path: "player4.png"
     },
     {
         name: "Manolis",
         role: "",
-        avatar_path: ""
+        avatar_path: "player5.png"
     },
     {
         name: "Kosmas",
         role: "",
-        avatar_path: ""
+        avatar_path: "pleyer6.png"
     },
         {
         name: "Renata",
         role: "",
-        avatar_path: ""
+        avatar_path: "player7.png"
     }
 ]
 
