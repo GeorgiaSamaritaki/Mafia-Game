76e3d7d7a32396a16cfdb2b88ae7e1ef923d9110
Antonis Prinianakis <csd3833@csd.uoc.gr>
2019-11-21 00:11:29 +0200

    Table sign in added without players joining

---
+++ frontend/src/app/pages/signin-table/signin-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ami-fullstack-signin-table',
+  templateUrl: './signin-table.component.html',
+  styleUrls: ['./signin-table.component.scss']
+})
+export class SigninTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
