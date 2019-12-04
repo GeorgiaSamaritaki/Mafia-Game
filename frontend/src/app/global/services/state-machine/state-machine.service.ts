7c5fd814e695b3938ae73d5278968b03de2d4c65
Antonis Prinianakis <csd3833@csd.uoc.gr>
2019-12-04 16:56:05 +0200

    state machine export

--- frontend/src/app/global/services/state-machine/state-machine.service.ts
+++ frontend/src/app/global/services/state-machine/state-machine.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { UserModel } from '../../models';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import * as _ from 'lodash';
 
