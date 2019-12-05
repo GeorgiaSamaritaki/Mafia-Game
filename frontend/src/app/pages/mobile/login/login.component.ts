0cc0335f722f14639c95a555293f5c83a4fa3fd8
Georgia Samaritaki <csd3840@csd.uoc.gr>
2019-12-05 02:43:41 +0200

    view enhancements and various fixes

--- frontend/src/app/pages/mobile/login/login.component.ts
+++ frontend/src/app/pages/mobile/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService, SocketsService } from 'src/app/global/services';
 import { UserModel } from 'src/app/global/models';
+import { StateMachineService } from 'src/app/global/services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ami-fullstack-login',
@@ -9,13 +11,55 @@ import { UserModel } from 'src/app/global/models';
 })
 export class LoginComponent implements OnInit {
   public selectedavatarindex: number;
-  constructor(
-    private usersService: UsersService
-  ) { }
+  public myUserID;
+  public userIDToTreat;
+  public foodToTreat;
+  public socketEvents: { event: string, message: any }[];
+
+  constructor(private statemachineService: StateMachineService,
+    private socketService: SocketsService,
+    private usersService: UsersService,
+    private router: Router
+  ) {
+    this.socketEvents = [];
+  }
+
+  async isWaiting() {
+    console.log("ok");
+    return (await this.statemachineService.getPhase().toPromise() === "Waiting");
+  }
 
   ngOnInit() {
+    this.myUserID = localStorage.getItem("username");
+    console.log("User id: "+this.myUserID);
+    // if (this.myUserID != null) this.goHome();
+
     this.selectedavatarindex = 0;
+    if (!this.isWaiting()) this.goHome();
+
+    this.userIDToTreat = "trix";
+    this.socketService.syncMessages("treating").subscribe(msg => {
+      if (msg.message.userID == this.myUserID) {
+        this.treated(msg.message.food);
+        this.socketEvents.push(msg);
+      }
+    });
   }
+
+  public treatsb() {
+    this.myUserID = (<HTMLInputElement>document.getElementById("inputname")).value;
+    this.foodToTreat = "i treat you this from " + this.myUserID;
+    this.statemachineService.treatsb(this.foodToTreat, this.userIDToTreat).subscribe();
+  }
+
+  goHome() {
+    this.router.navigate(['/mobile/mainpage']);
+  }
+
+  public treated(message) {
+    console.log("treated");
+  }
+
   selectedavatar(index: number) {
     this.selectedavatarindex = index;
   }
@@ -24,26 +68,26 @@ export class LoginComponent implements OnInit {
   }
   async addUser() {
     (<HTMLElement>document.getElementById("alert")).innerHTML = "";
-    var username: string = (<HTMLInputElement>document.getElementById("inputname")).value;
-    if (username == "" || await this.usernameExists(username)) {
-      (<HTMLElement>document.getElementById("alert")).innerHTML = "Invalid Username";
-      return;
+    this.myUserID = (<HTMLInputElement>document.getElementById("inputname")).value;
+    if (this.myUserID == "") {
+
+      (<HTMLElement>document.getElementById("alert")).innerHTML = "Empty Username";
     }
-    var index= 1 + this.selectedavatarindex;
-    console.log(
-      await this.usersService.addUser(
-        username,
-        ""
-        , "player" + index + ".png"
-      ).toPromise()
-    );
+    else if (await this.usernameExists(this.myUserID)) {
+      (<HTMLElement>document.getElementById("alert")).innerHTML = "Username already taken";
+    } else {
 
-    // if (sessionStorage.getItem('username'))
-    //   console.log(sessionStorage.getItem('username'));
-    // else {
-    //   console.log('New session');
-    //   sessionStorage.setItem('username', username);
-    // }
+      var index = 1 + this.selectedavatarindex;
+      console.log(
+        await this.usersService.addUser(
+          this.myUserID,
+          ""
+          , "player" + index + ".png"
+        ).toPromise()
+      );
+      localStorage.setItem("username", this.myUserID);
+      this.goHome();
 
+    }
   }
 }
