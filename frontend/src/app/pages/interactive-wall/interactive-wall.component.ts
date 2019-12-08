8e459243bd985b0b374d9dfad828a09fc1d08086
Georgia Samaritaki <csd3840@csd.uoc.gr>
2019-12-08 15:46:23 +0200

    ignore fixes

--- frontend/src/app/pages/interactive-wall/interactive-wall.component.ts
+++ frontend/src/app/pages/interactive-wall/interactive-wall.component.ts
@@ -15,13 +15,22 @@ export class InteractiveWallComponent implements OnInit {
   background_icon: string;
   round_histroy: string[];
   phases: string[];
+  responses: Map<string, string> = new Map([
+    ['Waiting', ''],
+    ['Open Ballot', 'The sun rises in Palermo, please, open your eyes.'],
+    ['Mafia Voting', 'It is time for the Mafia to wake, and choose their next victim.'],
+    ['Secret Voting', 'It is time to vote! Please, turn to your devices.'],
+    ['Doctor', 'It is time for the Doctor to wake, and choose the player they want to save.'],
+    ['Detective', 'It is time for the Detective to wake, and choose whose identity they want to know.'],
+    ['Barman', 'It is time for the Barman to wake, and choose whose abilities they want to cancel.']
+  ]);
   constructor(private statemachineService: StateMachineService,
     private socketService: SocketsService,
     private speakerService: SmartSpeakerService) {
-    this.lap = 0;
+    this.lap = 0;//this is backend
     this.phases_num = 0;
-    this.backgroundColor = '#E74C3C';
-    this.background_icon = "day";
+    this.backgroundColor = '#E74C3C';//TODO:this is css 
+    this.background_icon = "day";//TODO:this is css 
     this.round_histroy = [];
     this.phases = [];
     this.speakerService.addCommand('What day is it', () => {
@@ -30,9 +39,12 @@ export class InteractiveWallComponent implements OnInit {
     this.speakerService.addCommand('On which round are we', () => {
       this.speakerService.speak('We are on round ' + this.round);
     });
+    this.speakerService.addCommand('Repeat', () => {
+      this.speakerService.speak(this.responses[this.round]);
+    });
   }
 
-  nextPhase() {
+  nextPhase() { //TODO:this is css 
     console.log(this.lap);
     if (this.lap > 1) {
       if (this.isDay()) {
@@ -52,7 +64,7 @@ export class InteractiveWallComponent implements OnInit {
   manualChange() { //async
     this.lap++;
     console.log("Laps: " + this.lap);
-    if (this.lap == 1) {
+    if (this.lap == 1) { //this is waiting phase
       this.speakerService.speak('Hi! I am Smart Speaker. I will be your narrator for this game.');
       console.log('welcome speech');
     }
@@ -74,30 +86,30 @@ export class InteractiveWallComponent implements OnInit {
         this.phases_num++;
         this.phases.push('Night');
         this.speakerService.speak('The night falls in Palermo, please, close your eyes.');
-        this.speakerService.speak('It is time for the Mafia to wake, and choose their next victim.');
+        this.speakerService.speak(this.responses[this.round]);
         break;
       case 'Mafia Voting': //Secret Voting -> Mafia Voting
         this.round = 'Doctor';
-        this.speakerService.speak('It is time for the Doctor to wake, and choose the player they want to save.');
+        this.speakerService.speak(this.responses[this.round]);
         break;
       case 'Doctor': //Mafia Voting -> Doctor
         this.round = 'Detective';
-        this.speakerService.speak('It is time for the Detective to wake, and choose whose identity they want to know.');
+        this.speakerService.speak(this.responses[this.round]);
         break;
       case 'Detective': //Doctor -> Detective
         this.round = 'Barman';
-        this.speakerService.speak('It is time for the Barman to wake, and choose whose abilities they want to cancel.');
+        this.speakerService.speak(this.responses[this.round]);
         break;
       case 'Barman': //Detective -> Barman
         this.round = 'Open Ballot';
         this.nextPhase();
         this.phases_num++;
         this.phases.push('Day');
-        this.speakerService.speak('The sun rises in Palermo, please, open your eyes.');
+        this.speakerService.speak(this.responses[this.round]);
         break;
       case 'Open Ballot': //Barman -> Open Ballot
         this.round = 'Secret Voting';
-        this.speakerService.speak('It is time to vote! Please, turn to your devices.');
+        this.speakerService.speak(this.responses[this.round]);
         break;
     }
     this.round_histroy.push(this.round);
